d5ac998a4eccd9244cd211dd9269313e08273cd3
rajeshark <101121907+rajeshark@users.noreply.github.com>
2025-10-18 17:14:23 +0530

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -98,7 +98,7 @@ resource "aws_security_group" "RDS-sg" {
         from_port   = 5432
         to_port     = 5432
         protocol    = "tcp"
-        security_groups = [aws_security_group.eks-sg.id,aws_security_group.eks_fargate_sg.id]
+        security_groups = [aws_security_group.eks_cluster_sg.id,aws_security_group.eks_fargate_sg.id]
         
     }
     
